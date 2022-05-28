43a876cf0230711d1d9861bcd4d4e8baab2e036b
unknown <ashuzamaheshe4@gmail.com>
2022-05-28 14:31:14 +0200

    other commit

--- database/migrations/2022_05_28_122541_create_students_table.php
+++ database/migrations/2022_05_28_122541_create_students_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('phone');
             $table->timestamps();
         });
     }
