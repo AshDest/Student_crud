72f7c984def2b5ae9796cc495e3d9446e204e2df
unknown <ashuzamaheshe4@gmail.com>
2022-05-31 16:54:00 +0200

    other commit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/students', Students::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
