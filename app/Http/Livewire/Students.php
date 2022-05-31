74b5ff9326c3358072cd0167ac02e0e8df6506f0
unknown <ashuzamaheshe4@gmail.com>
2022-05-31 15:12:19 +0200

    other commit

--- app/Http/Livewire/Students.php
+++ app/Http/Livewire/Students.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Student;
 use Livewire\Component;
 
 class Students extends Component
 {
     public function render()
     {
+        $students = Student::orderBy('id','DESC')->get();
         return view('livewire.students');
     }
 }
