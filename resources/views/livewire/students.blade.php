ca408f8fab921ddd7c4c82cefa7d9e821541b98a
unknown <ashuzamaheshe4@gmail.com>
2022-05-31 15:40:34 +0200

    other commit

--- resources/views/livewire/students.blade.php
+++ resources/views/livewire/students.blade.php
@@ -4,7 +4,29 @@
             <div class="row">
                 <div class="card">
                     <div class="card-header">
-
+                        <h1>All Students</h1>
+                    </div>
+                    <div class="cad-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($students as $student)
+                                    <tr>
+                                        <td>{{$tudent->firstname}}</td>
+                                        <td>{{$tudent->lastname}}</td>
+                                        <td>{{$tudent->email}}</td>
+                                        <td>{{$tudent->phone}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
