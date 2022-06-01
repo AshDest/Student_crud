21fae966995b281963a086b58de01d1ed7aaf77c
unknown <ashuzamaheshe4@gmail.com>
2022-06-01 21:32:17 +0200

    other commit

--- resources/views/livewire/students.blade.php
+++ resources/views/livewire/students.blade.php
@@ -2,31 +2,33 @@
     <section>
         <div class="container">
             <div class="row">
-                <div class="card">
-                    <div class="card-header">
-                        <h1>All Students</h1>
-                    </div>
-                    <div class="cad-body">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Email</th>
-                                    <th>Phone</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($students as $student)
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h1>All Students</h1>
+                        </div>
+                        <div class="cad-body">
+                            <table class="table table-striped">
+                                <thead>
                                     <tr>
-                                        <td>{{$student->firstname}}</td>
-                                        <td>{{$student->lastname}}</td>
-                                        <td>{{$student->email}}</td>
-                                        <td>{{$student->phone}}</td>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($students as $student)
+                                        <tr>
+                                            <td>{{$student->firstname}}</td>
+                                            <td>{{$student->lastname}}</td>
+                                            <td>{{$student->email}}</td>
+                                            <td>{{$student->phone}}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
