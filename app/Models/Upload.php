b1103e67ffe87ca65191220ee75168c52b4d0952
unknown <ashuzamaheshe4@gmail.com>
2022-06-04 10:17:05 +0200

    other commit

---
+++ app/Models/Upload.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Upload extends Model
+{
+    use HasFactory;
+
+    protected $table = "uploads";
+
+    protected $fillable = ['title','filename'];
+}
