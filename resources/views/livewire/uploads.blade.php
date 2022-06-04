64becbccd8dd780d66dcc5f307ad33e83e9f27b5
unknown <ashuzamaheshe4@gmail.com>
2022-06-04 10:33:25 +0200

    other commit

---
+++ resources/views/livewire/uploads.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <section class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>File Upload</h3>
+                    </div>
+                    <div class="card-body">
+                        <form id="form-upload" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input type="text" name="title" class="form-control"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="filename">File</label>
+                                <input type="file" name="filename" class="form-control"/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Students;
+use App\Http\Livewire\Uploads;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/students', Students::class);
+Route::get('/uploads', Uploads::class);
 
 Auth::routes();
 
