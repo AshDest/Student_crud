cfc40d7a3b3bba1c7b666130971745bdb1ec346a
unknown <ashuzamaheshe4@gmail.com>
2022-06-05 13:11:50 +0200

    other commit

--- app/Http/Livewire/Images.php
+++ app/Http/Livewire/Images.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class Images extends Component
 {
+    public $images = [];
+
+    public function uploadImages()
+    {
+        foreach($this->images as $key=>$images){
+            $this->images[$key] = $images->store('images', 'public');
+        }
+    }
     public function render()
     {
         return view('livewire.images');
--- resources/views/livewire/images.blade.php
+++ resources/views/livewire/images.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <section>
+    <section style="padding-top: 2%;">
         <div class="container">
             <div class="col-md-6 offset-md-3">
                 <div class="card">
@@ -9,8 +9,10 @@
                     <div class="card-body">
                         <form id="upload-images" enctype="multipart/form-data">
                             <div class="form-group">
-                                <label for="filename">Files</label>
+                                <label for="images">Choose Images</label>
+                                <input type="file" name="images" class="form-control"/>
                             </div>
+                            <button type="submit" class="btn btn-success float-right">Upload</button>
                         </form>
                     </div>
                 </div>
